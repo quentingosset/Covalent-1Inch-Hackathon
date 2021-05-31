640856fd7d91222485d990a1bc1b4849c4573be3
Quentin <quentingosset7500@hotmail.com>
2021-05-31 09:32:20 +0200

    Update commande cron

--- app/Console/Commands/hackathon.php
+++ app/Console/Commands/hackathon.php
@@ -2,11 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Hackhaton;
 use App\Models\Pool;
 use App\Models\PoolContract;
 use App\Models\Token;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class hackathon extends Command
 {
@@ -41,6 +43,30 @@ class hackathon extends Command
      */
     public function handle()
     {
-
+        $contents = Storage::get('DnD1INCHAPI/challenge1_eth_data.json');
+        Hackhaton::create([
+            'challenge' => 1,
+            'data' => $contents,
+            'network' => 1
+        ]);
+        $contents = Storage::get('DnD1INCHAPI/challenge1_bsc_data.json');
+        Hackhaton::create([
+            'challenge' => 1,
+            'data' => $contents,
+            'network' => 56
+        ]);
+        $contents = Storage::get('DnD1INCHAPI/challenge2_eth_data.json');
+        Hackhaton::create([
+            'challenge' => 2,
+            'data' => $contents,
+            'network' => 1
+        ]);
+        $contents = Storage::get('DnD1INCHAPI/challenge2_eth_data.json');
+        Hackhaton::create([
+            'challenge' => 2,
+            'data' => $contents,
+            'network' => 56
+        ]);
+        return 1;
     }
 }
