3f3591c9c68cd5fc1799a4ada370fd98a3234aec
Quentin <quentingosset7500@hotmail.com>
2021-05-31 10:15:52 +0200

    Update commande cron

--- resources/views/hackathon/stats.blade.php
+++ resources/views/hackathon/stats.blade.php
@@ -5,12 +5,8 @@
 <head>
     <base href="../../">
     <meta charset="utf-8" />
-    <title>Metronic Theme | Keenthemes</title>
-    <meta name="description" content="Craft admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
-    <meta name="keywords" content="Craft, bootstrap, Angular 10, Vue, React, Laravel, admin themes, free admin themes, bootstrap admin, bootstrap dashboard" />
-    <link rel="canonical" href="Https://preview.keenthemes.com/start" />
+    <title>dungeons & data - 1inch</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
